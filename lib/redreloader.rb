875d5ab199ca5fcf1118cd31ad41f480906458d6
Andrew Snow <andrew@modulus.org>
2015-11-21 13:17:36 +1100

    v0.3. use Pub/sub to watch for changes instead of polling

--- lib/redreloader.rb
+++ lib/redreloader.rb
@@ -3,41 +3,55 @@ require 'zlib'
 require 'digest/md5'
 
 class Redreloader
-  class << self
-    attr_accessor :redis
-    attr_accessor :ttl
-    
-    def [](key)
-      val = with_binary_redis { @redis.getrange(key, 16, -1) }
-      Zlib::Inflate.inflate(val) if val && val != ""
-    end
-    
-    def []=(key, val)
-      with_binary_redis { @redis.set(key, calcmd5(val) + Zlib::Deflate.deflate(val), ex: @ttl) }
-    end
+  def initialize(redis_config)
+    @redis = Redis.new(redis_config)
+    @redis_sub = Redis.new(redis_config)
+  end
 
-    def calcmd5(val)
-      Digest::MD5.digest val
-    end
+  def [](key)
+    val = with_binary_redis { @redis.getrange(key, 16, -1) }
+    Zlib::Inflate.inflate(val) if val && val != ""
+  end
 
-    def digest(key)
-      data = with_binary_redis { @redis.getrange(key, 0, 15) }
-      data if data != ""
-    end
+  def []=(key, val)
+  with_binary_redis { @redis.set(key, self.class.calcmd5(val) + Zlib::Deflate.deflate(val)) }
+end
 
-    def if_changed_from(key, old_digest)
-      if (new_digest = digest(key)) && new_digest != old_digest
-        newdata = self[key]
-        yield(newdata, new_digest) if newdata
+  def self.calcmd5(val)
+    Digest::MD5.digest val
+  end
+
+  def digest(key)
+    data = with_binary_redis { @redis.getrange(key, 0, 15) }
+    data if data != ""
+  end
+
+  def process_changes(key, old_digest, &bl)
+    @redis_sub.subscribe("__keyspace@0__:#{key}") do |on|
+      on.message do |channel, message|
+        old_digest = yield_if_changed(key, old_digest, &bl)
+      end
+
+      on.subscribe do |channel, message|
+        # run it here to avoid race condition of change between check and subscribe
+        old_digest = yield_if_changed(key, old_digest, &bl)
       end
     end
-    
-    def with_binary_redis
-      original_encoding = Encoding.default_external
-      Encoding.default_external = Encoding.find('binary')
-      yield
-    ensure
-      Encoding.default_external = original_encoding
+  end
+
+  def yield_if_changed(key, old_digest, &bl)
+    if (new_digest = digest(key)) && new_digest != old_digest
+      newdata = self[key]
+      bl.call(newdata, new_digest) if newdata
     end
+    new_digest
+  end
+
+  def with_binary_redis
+    original_encoding = Encoding.default_external
+    Encoding.default_external = Encoding.find('binary')
+    yield
+  ensure
+    Encoding.default_external = original_encoding
   end
 end
